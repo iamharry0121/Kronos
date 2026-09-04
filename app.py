b374268378aec4a5c06605ba660f055048e761dd
Harish <harinavanee2011@gmail.com>
2026-09-04 09:40:12 -0400

    Make delete button functional

--- app.py
+++ app.py
@@ -1,29 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_wtf import FlaskForm, CSRFProtect
 from wtforms import SubmitField, StringField 
 from wtforms.validators import DataRequired
-import secrets
+import database_manager as db_mgr
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'testingtesting6767'
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///tasks.db'
+
+db_mgr.init_db(app)
 
 class EnterTask(FlaskForm):
     task = StringField('Task', [DataRequired()])
     submit = SubmitField('Submit')
 
-app = Flask(__name__)
-foo = secrets.token_urlsafe(16) #URL in urlsafe meaning it only uses characters found in a web url
-app.secret_key = foo
-csrf = CSRFProtect(app)
-etls = []
-
-
 @app.route('/', methods=['GET', 'POST'])
 def main_page():
     form = EnterTask()
     if form.validate_on_submit():
-        task = form.task.data
-        etls.append(task)
-        return render_template('main_page.html', form=form, values=etls)
+        db_mgr.add_task(form.task.data)
+        return redirect(url_for('main_page'))
 
-    return render_template('main_page.html', form=form)
-        
+    tasks = db_mgr.get_all_tasks()
+    return render_template('main_page.html', form=form, values=tasks)
+
+@app.route('/delete/<int:task_id>', methods=['POST'])
+def delete_task(task_id):
+    db_mgr.delete_task_by_id(task_id)
+    return redirect(url_for('main_page'))
+
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ database_manager.py
@@ -0,0 +1,33 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+db = SQLAlchemy()
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(150), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    completed = db.Column(db.Boolean, default=False)
+
+def init_db(app):
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+
+def add_task(task_text):
+    new_task = Task(task=task_text)
+    db.session.add(new_task)
+    db.session.commit()
+
+def get_all_tasks():
+    return Task.query.all() 
+
+def delete_task_by_id(task_id):
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+
+def toggle_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    task.completed = not task.completed
+    db
+
