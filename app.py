4c4b56e8c223ee8ad2c8be852fb2addf2eb5a0dd
Harish <harinavanee2011@gmail.com>
2026-09-05 18:40:25 -0400

    Add Login/Signup capability and User Based Task Menu

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, redirect, url_for, jsonify
+from flask import Flask, render_template, redirect, url_for, jsonify, flash
 from flask_wtf import FlaskForm, CSRFProtect
-from wtforms import SubmitField, StringField 
-from wtforms.validators import DataRequired
+from wtforms import SubmitField, StringField , PasswordField
+from wtforms.validators import DataRequired, Email
+from flask_login import (
+    LoginManager, login_user, logout_user, login_required, current_user
+)
 import database_manager as db_mgr
 
 app = Flask(__name__)
@@ -11,32 +14,93 @@ app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///tasks.db'
 csrf = CSRFProtect(app)
 db_mgr.init_db(app)
 
+#flask login setup + flask forms
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db_mgr.get_user_by_id(user_id)
+
 class EnterTask(FlaskForm):
     task = StringField('Task', [DataRequired()])
     submit = SubmitField('Submit')
+
+class SignupForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign up')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log in')
 
 @app.route('/')
 def landing_page():
     return render_template('landing.html')
 
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for('main_page'))
+
+    form = SignupForm()
+    if form.validate_on_submit():
+        existing_user = db_mgr.get_user_by_email(form.email.data)
+        if existing_user:
+            flash('Email already exists.')
+            return redirect(url_for('signup'))
+
+        new_user = db_mgr.create_user(form.email.data, form.password.data)
+
+        login_user(new_user)
+        return redirect(url_for('main_page'))
+
+    return render_template('signup.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main_page'))
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = db_mgr.get_user_by_email(form.email.data)
+
+        if user is None or not user.checkpassword(form.password.data):
+            flash('Incorrect email or password')
+            return redirect(url_for('login'))
+
+        login_user(user)
+        return redirect(url_for('main_page'))
+
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('landing_page'))
+
 @app.route('/home', methods=['GET', 'POST'])
 def main_page():
     form = EnterTask()
     if form.validate_on_submit():
-        db_mgr.add_task(form.task.data)
+        db_mgr.add_task(form.task.data, current_user.id)
         return redirect(url_for('main_page'))
 
-    tasks = db_mgr.get_all_tasks()
+    tasks = db_mgr.get_all_tasks(current_user.id)
     return render_template('main_page.html', form=form, values=tasks)
 
 @app.route('/toggle/<int:task_id>', methods=['POST'])
 def toggle_task(task_id):
-    is_completed = db_mgr.toggle_task(task_id)
+    is_completed = db_mgr.toggle_task(task_id, current_user.id)
     return jsonify({'success':True, 'completed':is_completed})
 
 @app.route('/delete/<int:task_id>', methods=['POST'])
 def delete_task(task_id):
-    db_mgr.delete_task_by_id(task_id)
+    db_mgr.delete_task_by_id(task_id, current_user.id)
     return jsonify({'success':True})
 
 if __name__ == '__main__':
--- database_manager.py
+++ database_manager.py
@@ -1,6 +1,22 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
+
 db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    #one row is one account and usermixin gives all auth based functions so dont write auth things
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(250), nullable=False)
+
+    def set_password(self, plain_txt_pwd):
+        self.password_hash = generate_password_hash(plain_txt_pwd)
+
+    def check_password(self, plain_txt_pwd):
+        return check_password_hash(self. password_hash, plain_txt_pwd)
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,27 +24,46 @@ class Task(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     completed = db.Column(db.Boolean, default=False)
 
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
 def init_db(app):
     db.init_app(app)
     with app.app_context():
         db.create_all()
 
-def add_task(task_text):
-    new_task = Task(task=task_text)
+#user related helpers
+def get_user_by_email(email):
+    return User.query.filter_by(email=email).first()
+
+def get_user_by_id(user_id):
+    return User.query.get(int(user_id))
+
+def create_user(email, plain_txt_pwd):
+    new_user = User(email=email)
+    new_user.set_password(plain_txt_pwd)
+    db.session.add(new_user)
+    db.session.commit()
+    return new_user
+#end of user related helpers
+
+#task related helpers
+def add_task(task_text, user_id):
+    new_task = Task(task=task_text, user_id=user_id)
     db.session.add(new_task)
     db.session.commit()
 
-def get_all_tasks():
-    return Task.query.all() 
+def get_all_tasks(user_id):
+    return Task.query.filter_by(user_id=user_id).all()
 
-def delete_task_by_id(task_id):
-    task = Task.query.get_or_404(task_id)
+def delete_task_by_id(task_id, user_id):
+    task = Task.query.filter_by(id=task_id, user_id=user_id).first_or_404()
     db.session.delete(task)
     db.session.commit()
 
-def toggle_task(task_id):
-    task = Task.query.get_or_404(task_id)
+def toggle_task(task_id, user_id):
+    task = Task.query.filter_by(id=task_id, user_id=user_id).first_or_404()
     task.completed = not task.completed
     db.session.commit()
     return task.completed
+#end of task related helpers
 
